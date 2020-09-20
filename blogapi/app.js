20be062fb96112851086a13f75016da96496a30e
patreva <patrickwahome74@gmail.com>
2020-09-20 15:56:23 +0300

    adding files

---
+++ blogapi/app.js
@@ -0,0 +1,77 @@
+const path = require('path');
+// const fs = require('fs');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+// const helmet = require('helmet');
+// const compression = require('compression');
+// const morgan = require('morgan');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+const { Socket } = require('dgram');
+
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'),{flags:"a"});
+
+// app.use(helmet());
+// app.use(compression());
+// app.use(morgan('combined', {stream: accessLogStream}));
+
+const app = express();
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'images');
+    },
+    filename: (req, file, cb)=>{ 
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+    }
+}); 
+
+const fileFilter = (req, file, cb)=>{
+    if(file.mimetype === 'image/png' || file.mimetype === 'image.jpg' || file.mimetype === 'image/jpeg'){
+        cb(null, true);
+    }else{
+        cb(null, false);
+    }
+}
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes); 
+app.use('/auth', authRoutes); 
+
+app.use((error,req,res,next)=>{
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data
+    res.status(status).json({message: message, data: data});
+});
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.6qqoq.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`,
+{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(result=>{
+    console.log('connected');
+    const server = app.listen(process.env.PORT || 8080);
+    const io = require('./socket').init(server);
+    io.on('connection',socket=>{
+        console.log('Client connected')
+    });
+})
+.catch(err=>console.log(err));
